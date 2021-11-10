d8ea4721506e8c362e023ab5f3aa02558322ab24
Rayan Reynaldo <fsdevrayan@gmail.com>
2021-11-10 21:17:26 +0800

    Update App.jsx

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -31,9 +31,9 @@ import { CUSTOM } from './classes/custom'
 import ExampleImages from './ExampleImages'
 import noPhoto from './assets/no-image.jpg'
 
-// const base_url = "https://cbidemo.netlify.app"
+const base_url = "https://cbidemo.netlify.app"
 // const base_url = "http://localhost:3000" 
-const base_url = "http://192.168.1.18:3000"
+// const base_url = "http://192.168.1.18:3000"
 
 const defaultText = "No image to classify"
 
